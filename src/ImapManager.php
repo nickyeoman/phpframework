c504abbe55a9f54e6004997397a87cd501e1199e
Nick Yeoman <c@nickyeoman.com>
2022-10-07 09:37:44 -0700

    fixed the imap class
    it now kinda works

--- src/ImapManager.php
+++ src/ImapManager.php
@@ -1,42 +1,78 @@
 <?php
 namespace Nickyeoman\Framework;
+use \Exception;
 
 /**
 * IMAP Class
-* v0.1 (beta)
-*
-* Just a helper for barbushin/php-imap
+* v0.2 (beta)
 **/
 
 class ImapManager {
 
-  public $mailbox = new stdClass();
+  public $mailbox = null; //class
   public $mailids = '';
+  public $mails = array();
 
   public function __construct() {
 
-    $this->mailbox = new PhpImap\Mailbox($_ENV['IMAP_SERVER'],$env['IMAP_USER'],$env['IMAP_PASSWORD']);
-    $this->mailbox->setAttachmentsIgnore(true);
+    // Check if the server has imap working (docker should work)
+    if(!function_exists('imap_open')){
+      throw new Exception('IMAP is not enabled!');
+    }
 
+
+    $this->mailbox = imap_open($_ENV['IMAP_SERVER'], $_ENV['IMAP_USER'], $_ENV['IMAP_PASSWORD']) 
+      or die('unable to connect Email: ' . imap_last_error());
+    
+    // list of mail ids
+    $this->mailids = imap_search($this->mailbox, 'ALL'); //https://www.php.net/manual/en/function.imap-search.php
+    
   }
 
   public function disconnect(){
-    $this->mailbox->disconnect();
+    imap_close($this->mailbox);
   }
 
   public function getEmails(){
 
-    try {
-    	// Search in mailbox folder for specific emails
-    	// PHP.net imap_search criteria: http://php.net/manual/en/function.imap-search.php
-    	// Here, we search for "all" emails
-    	$this->mailids = $mailbox->searchMailbox('ALL');
-    } catch(PhpImap\Exceptions\ConnectionException $ex) {
-    	echo "IMAP connection failed: " . $ex;
-    	die();
+    if ($this->mailids) {
+
+      rsort($this->mailids);
+
+      foreach ($this->mailids as $email_number) {
+        $headers = imap_fetch_overview($this->mailbox, $email_number, 0);
+
+        /*  Returns a particular section of the body*/
+        $message = imap_fetchbody($this->mailbox, $email_number, '1');
+        $structure = imap_fetchstructure($this->mailbox, $email_number);
+
+        // Email body decoding
+        // https://www.learn-codes.net/php/php-email-body-decoding/
+        if(isset($structure->parts) && is_array($structure->parts) && isset($structure->parts[1])) {
+          $part = $structure->parts[1];
+          if($part->encoding == 3) {
+              $message = imap_base64($message);
+          }else if($part->encoding == 1) {
+              $message = imap_8bit($message);
+          }else{
+              $message = imap_qprint($message);
+          }
+        }
+
+        $this->mails[$email_number] = array(
+          'subject' => $headers[0]->subject
+          ,'from' => $headers[0]->from
+          ,'date' => $headers[0]->date
+          ,'body' => utf8_encode($message)
+        );
+
+      }
+
+      return $this->mails;
+                        
     }
 
-    dump($this->mailids);
+
 
   }
 }
