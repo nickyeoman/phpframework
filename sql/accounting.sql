42f380c63597a0f62a882679f6db75b3e9a076bd
Nick Yeoman <c@nickyeoman.com>
2023-02-13 14:25:56 -0800

    updated accounting database and updated composer yml file

--- sql/accounting.sql
+++ sql/accounting.sql
@@ -9,30 +9,18 @@ CREATE TABLE transactions (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   transaction_date DATE NOT NULL,
   transaction_description VARCHAR(255) NOT NULL,
-  transaction_amount DECIMAL(10, 2) NOT NULL
+  transaction_total DECIMAL(10, 2) NOT NULL
 );
 
-CREATE TABLE journal_entries (
+CREATE TABLE receipts (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  transaction_id INT NOT NULL,
-  account_id INT NOT NULL,
-  entry_type ENUM('Debit', 'Credit') NOT NULL,
-  entry_amount DECIMAL(10, 2) NOT NULL,
-  FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id),
-  FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+  `internal_id` int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  `external_id` int(11),
+  `receipt_date` datetime NOT NULL
 );
 
-CREATE TABLE parties (
+CREATE TABLE `receipts_transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  party_name VARCHAR(255) NOT NULL,
-  party_type ENUM('Customer', 'Supplier', 'Employee') NOT NULL
-);
-
-CREATE TABLE ledger (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  account_id INT NOT NULL,
-  ledger_date DATE NOT NULL,
-  debit_amount DECIMAL(10, 2) NOT NULL,
-  credit_amount DECIMAL(10, 2) NOT NULL,
-  FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+  `receipts_id` int(11) UNSIGNED DEFAULT NULL,
+  `transactions_id` int(11) UNSIGNED DEFAULT NULL
 );
